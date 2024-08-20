dfe67844184136d19125e9c281f5ff54e827b501
jonjno <sheeja4106@gmail.com>
2024-08-20 04:48:11 -0700

    first commit

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,7 @@ app.use("/api/auth", authroutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
