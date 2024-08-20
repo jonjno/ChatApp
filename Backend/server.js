e124ee6aca681ab8c70a0dfefcfc89decd0990a5
jonjno <sheeja4106@gmail.com>
2024-08-20 04:33:12 -0700

    chnages

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ import authroutes from "./routes/authroutes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import path from "path";
 
 import { app, server } from "./socket/socket.js";
 
@@ -15,6 +16,8 @@ app.use(express.json());
 // to parse the incomming request with json payload(from req.body)
 
 const Port = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 // console.log(process.env.PORT);
 // app.get("/", (req, res) => {
 //   res.send("Hello World Sever Ready!");
@@ -23,6 +26,12 @@ const Port = process.env.PORT || 5000;
 app.use("/api/auth", authroutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(Port, () => {
   connectToMongoDB();
